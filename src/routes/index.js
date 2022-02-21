b2da6bb01e76a2367f8c1e23b7e79f08eec89d0e
Efosa <okoobohefosa@gmail.com>
2022-02-21 04:57:49 +0100

    add whoami route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,46 +1,55 @@
-const express = require('express')
-const router = express.Router()
-const {getUser, getAdmin, parseKey} = require('../utils').middleware
+const express = require("express");
+const router = express.Router();
+const { getUser, getAdmin, parseKey } = require("../utils").middleware;
 
-const { handleLogin } = require('../controllers/auth.controller')
-const handleRegister = require('../controllers/user.controller')
+const { handleLogin, whoami } = require("../controllers/auth.controller");
+const handleRegister = require("../controllers/user.controller");
 
-const {getAccountBalance, getAllBusinessBalances, creditBalance} = require('../controllers/balance.controller')
-const {getTransaction, getAllTransaction, getAllBusinessTransactions} = require('../controllers/transaction.controller')
-const {getPayment, getAllPayments, getAllBusinessPayments} = require('../controllers/payment.controller')
+const {
+  getAccountBalance,
+  getAllBusinessBalances,
+  creditBalance,
+} = require("../controllers/balance.controller");
+const {
+  getTransaction,
+  getAllTransaction,
+  getAllBusinessTransactions,
+} = require("../controllers/transaction.controller");
+const {
+  getPayment,
+  getAllPayments,
+  getAllBusinessPayments,
+} = require("../controllers/payment.controller");
 
-const {getPlans} = require('../controllers/plans.controller')
-const sendData = require('../controllers/sendData.controller')
+const { getPlans } = require("../controllers/plans.controller");
+const sendData = require("../controllers/sendData.controller");
 
-
-router.post('/auth', handleLogin)
-router.post('/users', handleRegister)
-
+router.get("/whoami", whoami);
+router.post("/auth", handleLogin);
+router.post("/users", handleRegister);
 
 // Dashboard Routes
 // Routes Called by businesses
-router.get('/plans', getUser, getPlans)
+router.get("/plans", getUser, getPlans);
 
-router.get('/balance', getUser, getAccountBalance)
+router.get("/balance", getUser, getAccountBalance);
 
-router.get('/transaction/:id', getUser, getTransaction)
-router.get('/transactions', getUser, getAllTransaction)
+router.get("/transaction/:id", getUser, getTransaction);
+router.get("/transactions", getUser, getAllTransaction);
 
-router.get('/payment/:id', getUser, getPayment)
-router.get('/payments', getUser, getAllPayments)
-
+router.get("/payment/:id", getUser, getPayment);
+router.get("/payments", getUser, getAllPayments);
 
 /**Routes Called by Admin
  * remember to set auth tokens to expire
  */
-router.get('/admin/balances', getAdmin, getAllBusinessBalances)
-router.get('/admin/transactions', getAdmin, getAllBusinessTransactions)
-router.get('/admin/payments', getAdmin, getAllBusinessPayments)
-router.post('/admin/credit', getAdmin, creditBalance)
-
+router.get("/admin/balances", getAdmin, getAllBusinessBalances);
+router.get("/admin/transactions", getAdmin, getAllBusinessTransactions);
+router.get("/admin/payments", getAdmin, getAllBusinessPayments);
+router.post("/admin/credit", getAdmin, creditBalance);
 
 /**END USER ROUTES
  */
-router.post('/buy', parseKey, sendData)
+router.post("/buy", parseKey, sendData);
 
-module.exports = router
+module.exports = router;
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,15 +1,27 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
-const { Account } = require('../models/account')
+const { Account } = require("../models/account");
 
 const auth = async (email, password) => {
-  const user = await Account.findOne({ email }).exec()
-  if (!user) return { status: 400, message: 'Invalid email or password.' }
+  const user = await Account.findOne({ email }).exec();
+  if (!user) return { status: 400, message: "Invalid email or password." };
 
-  const validPassword = await bcrypt.compare(password, user.password)
-  if (!validPassword) { return { status: 400, message: 'Invalid email or password.' } }
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    return { status: 400, message: "Invalid email or password." };
+  }
 
-  return { user }
-}
+  return { user };
+};
 
-module.exports = { auth }
+const whoami = async (email) => {
+  const user = await Account.findOne(
+    { email },
+    { password: 0, createdAt: 0, updatedAt: 0, __v: 0 }
+  ).exec();
+
+  if (!user) return { status: 404, message: "User not found" };
+  return { user };
+};
+
+module.exports = { auth, whoami };
