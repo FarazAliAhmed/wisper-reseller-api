60f12d344fde71df9654f1d3dd80a8c1190d601a
edwardoboh <obohedward@gmail.com>
2022-02-24 20:43:07 +0100

    fixed errors in config

--- config/production.js
+++ config/production.js
@@ -1,7 +1,5 @@
-const config = require("config");
-
 module.exports = {
-  db: config.util.getEnv("DB_URL"),
-  jwtSecret: config.util.getEnv("JWT_SECRET"),
-  port: config.util.getEnv("PORT"),
+  db: process.env.DB_URL,
+  jwtSecret: process.env.JWT_SECRET,
+  port: process.env.PORT,
 };
