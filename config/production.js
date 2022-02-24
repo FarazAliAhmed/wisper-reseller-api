41807c3b04dc662df467b4923bd6a1702a775646
edwardoboh <obohedward@gmail.com>
2022-02-24 20:33:11 +0100

    fix error with production config file

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 const config = require("config");
 
-export default {
+module.exports = {
   db: config.util.getEnv("DB_URL"),
   jwtSecret: config.util.getEnv("JWT_SECRET"),
   port: config.util.getEnv("PORT"),
