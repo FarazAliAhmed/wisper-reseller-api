4e93396a183b3b69aced471bac0baae487731a72
edwardoboh <obohedward@gmail.com>
2022-12-13 22:18:24 +0100

    feat: add check to block out inactive accounts

--- src/utils/middleware/parseKey.js
+++ src/utils/middleware/parseKey.js
@@ -4,7 +4,13 @@ const parseKey = async (req, res, next) => {
     if (req.headers && req.headers["x-api-key"]){
         const api_key = req.headers["x-api-key"]
         const account = await Account.findOne({access_token: api_key}).exec()
+
+        // Check account validity
         if (!account) return res.status(401).json({error: true, message: "invalid access token"})
+
+        // Check if account is enabled
+        if(!account.active) return res.status(400).json({error: true, message: "Account temporarily suspended. Contact Admin."})
+        
         req.user = account
         return next()
     }
