d32f543af552f3d1700ff40c1024d85b3b39de74
thexovc <osazeepeter79@gmail.com>
2023-08-13 14:06:06 +0100

    purchase mega data, added more auth

--- src/routes/megaPrice.route.js
+++ src/routes/megaPrice.route.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const getAdmin = require("../utils/middleware/getAdmin");
 const megaPriceController = require("../controllers/megaPrice.controller");
+const getUser = require("../utils/middleware/getUser");
 const router = express.Router();
 
-router.post("/editMegaPrice", megaPriceController.updateMegaPrice);
-router.post("/purchaseMegaData", megaPriceController.purchaseMegaData);
+router.post("/editMegaPrice", getAdmin, megaPriceController.updateMegaPrice);
+router.post("/purchaseMegaData", getUser, megaPriceController.purchaseMegaData);
 
 module.exports = router;
