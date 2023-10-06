00eed45316e3062754a1c85e3a3bf9af11e46b56
thexovc <osazeepeter79@gmail.com>
2023-10-06 19:30:45 +0100

    fixes

--- src/server.js
+++ src/server.js
@@ -50,7 +50,7 @@ app.use(
     extended: false,
   })
 );
-app.use("/public", express.static("public"));
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.status(200).send({ status: "healthy" });
