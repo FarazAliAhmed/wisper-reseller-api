bbb053a38d2d76d8082617a233bd14840f3eb7cf
thexovc <osazeepeter79@gmail.com>
2023-11-18 11:31:17 +0100

    fixes

--- src/services/balance.service.js
+++ src/services/balance.service.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Balance = require("../models/dataBalance");
-const BalanceEvent = require('../events/balance.event');
-const { balance_update } = require('../events/_eventTypes');
+const BalanceEvent = require("../events/balance.event");
+const { balance_update } = require("../events/_eventTypes");
 
 const getBalance = async (id) => {
-  const business_id = mongoose.Types.ObjectId(id)
-  const balance = await Balance.findOne({ business: business_id }).populate('business').exec();
+  const business_id = mongoose.Types.ObjectId(id);
+  const balance = await Balance.findOne({ business: business_id })
+    .populate("business")
+    .exec();
   if (balance) return { balance };
   return {
     status: 500,
@@ -15,7 +17,7 @@ const getBalance = async (id) => {
 };
 
 const getWallet = async (id) => {
-  const business_id = mongoose.Types.ObjectId(id)
+  const business_id = mongoose.Types.ObjectId(id);
   const balance = await Balance.findOne({ business: business_id }).exec();
   if (balance) return { balance };
   return {
@@ -26,7 +28,7 @@ const getWallet = async (id) => {
 };
 
 const getAllBalance = async () => {
-  const balances = await Balance.find().populate('business').exec();
+  const balances = await Balance.find().populate("business").exec();
   if (balances) return { balances };
   return { status: 500, message: `Unable to fetch balances of all accounts` };
 };
@@ -42,7 +44,8 @@ const create = async (id) => {
   return { status: 500, message: "Error creating wallet for business" };
 };
 
-const upgradeBalance = async (id) => { //change the unit from MB to ₦
+const upgradeBalance = async (id) => {
+  //change the unit from MB to ₦
   const balance = await Balance.findOneAndUpdate(
     { business: id },
     { data_unit: "MB", data_volume: 0 },
@@ -55,24 +58,26 @@ const upgradeBalance = async (id) => { //change the unit from MB to ₦
     status: 201,
     message: `Data Balance Unit Updated`,
   };
-}
+};
 
 // Made use of this as an alternative to migration. Should not be used in production
 const upgradeAllBalance = async () => {
-  const balance = await Balance.updateMany({},
-      {
-        $set : {
-          "mega_wallet": {
-                  mtn_sme: 0,
-                  mtn_gifting: 0,
-                  airtel: 0,
-                  glo: 0,
-                  unit: "MB"
-          }, data_unit: "₦" 
-        }
+  const balance = await Balance.updateMany(
+    {},
+    {
+      $set: {
+        mega_wallet: {
+          mtn_sme: 0,
+          mtn_gifting: 0,
+          airtel: 0,
+          glo: 0,
+          unit: "MB",
+        },
+        data_unit: "₦",
       },
-      {upsert:false}
-    ).exec();
+    },
+    { upsert: false }
+  ).exec();
 
   return {
     balance,
@@ -80,16 +85,15 @@ const upgradeAllBalance = async () => {
     status: 201,
     message: `All Balance Accounts upgraded`,
   };
-}
+};
 
 const credit = async (id, creditAmount, field) => {
-
-  const old_balance = await Balance.findOne({ business: id }).exec()
+  const old_balance = await Balance.findOne({ business: id }).exec();
   const balance = await Balance.findOneAndUpdate(
-      { business: id },
-      { $inc: { [field]: creditAmount } },
-      { new: true }
-    ).exec()
+    { business: id },
+    { $inc: { [field]: creditAmount } },
+    { new: true }
+  ).exec();
 
   // BalanceEvent.emit(balance_update, {new_balance: balance, old_balance})
 
@@ -109,25 +113,32 @@ const credit = async (id, creditAmount, field) => {
  *    the debitAmount value is negative(-)
  */
 const debit = async (id, debitAmount, field) => {
-  const old_balance = await Balance.findOne({ business: id }).exec()
+  console.log({ id, debitAmount, field });
+
+  const old_balance = await Balance.findOne({ business: id }).exec();
   const balance = await Balance.findOneAndUpdate(
     { business: id },
-    { $inc: { [field] : -1 * debitAmount } },
+    { $inc: { [field]: -1 * debitAmount } },
     { new: true }
   ).exec();
-  
-  const [parent, child] = field.split(".")
+
+  const [parent, child] = field.split(".");
   let checker, wallet_name;
-  
-  if(child){
-    checker = balance[parent][child]
-    wallet_name = `in ${child.toUpperCase().replace("_", " ")} wallet`
-  }else{
-    checker = balance[parent]
-    wallet_name = "in wallet"
+
+  if (child) {
+    checker = balance[parent][child];
+    wallet_name = `in ${child.toUpperCase().replace("_", " ")} wallet`;
+  } else {
+    checker = balance[parent];
+    wallet_name = "in wallet";
   }
 
-  if (checker < 0) return { error: true, status: 401, message: `Insufficient Balance ${wallet_name}` };
+  if (checker < 0)
+    return {
+      error: true,
+      status: 401,
+      message: `Insufficient Balance ${wallet_name}`,
+    };
   // if (debitAmount > 0) BalanceEvent.emit(balance_update, {new_balance: balance, old_balance});
 
   return { balance, error: false, status: 201 };
@@ -143,7 +154,7 @@ const reset = async (id) => {
         mtn_gifting: 0,
         airtel: 0,
         glo: 0,
-      }
+      },
     }
   ).exec();
   if (balance) return { balance };
